e69d9c94b8436247826b33169a84dce708819163
leasever <leandrovegaabraham@gmail.com>
2023-11-18 22:31:58 -0500

    lobby sections

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,6 +1,11 @@
 import ProductCarousel from "@/components/product/ProductCarousel";
 import Slide from "@/components/slide/Slide";
 import { getDataSlide } from "../services/slide.service";
+import LogoSection from "@/components/lobby/LogoSection";
+import Collection from "@/components/lobby/Collection";
+import NewsletterSection from "@/components/lobby/NewsletterSections";
+import PromoSection from "@/components/lobby/PromoSection";
+import Testimonials from "@/components/lobby/Testimonials";
 
 export default async function HomePage() {
   const { data: slides } = await getDataSlide();
@@ -9,6 +14,11 @@ export default async function HomePage() {
     <>
       <Slide data={slides} />
       <ProductCarousel />
+      <Collection />
+      <PromoSection />
+      <LogoSection />
+      <Testimonials />
+      <NewsletterSection />
     </>
   );
 }
