670d1d69da6b710342ca1487a9cd231f27495adf
leasever <leandrovegaabraham@gmail.com>
2024-01-10 16:33:46 -0500

    optimized img auth1

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-
-export default async function AuthLayout({
+"use client";
+export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
@@ -8,14 +7,12 @@ export default async function AuthLayout({
   return (
     <>
       <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden w-full h-full bg-muted lg:flex dark:border-r">
-          <Image
-            src="/auth.webp"
-            className="w-full object-cover"
+        <div className="relative hidden h-full bg-muted lg:flex dark:border-r">
+          <img
+            src="./auth.webp"
+            className=" w-full object-cover"
             alt="auth"
-            width={2732}
-            height={2532}
-            priority
+            loading="eager"
           />
         </div>
         <main>{children}</main>
