6d47f5899a75f17657dca181ad1943b6088bf2fa
leasever <leandrovegaabraham@gmail.com>
2024-01-10 06:40:21 -0500

    optimized single page 3

--- src/app/(ecommerce)/product/[slug]/page.tsx
+++ src/app/(ecommerce)/product/[slug]/page.tsx
@@ -1,10 +1,20 @@
-import { getDataProductBySlug } from "@/app/services/product.service";
+import {
+  getDataProductBySlug,
+  getDataProducts,
+} from "@/app/services/product.service";
 import ProductSinglePage from "@/components/product/ProductSinglePage";
 
 export interface ProductPageProps {
   params: {
     slug: string;
   };
+}
+
+export async function generateStaticParams() {
+  const products = await getDataProducts();
+  return products.data.map((product) => ({
+    slug: product.attributes.slug,
+  }));
 }
 
 export default async function ProductPage({ params }: ProductPageProps) {
--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,11 +1,6 @@
 import Slide from "@/components/slide/Slide";
 import { fetchDataFromApi } from "@/lib/api";
 
-// async function getDataSlide(): Promise<SlideInterface> {
-//   const res = await fetchDataFromApi(`/api/sliders?populate=*`);
-//   return res;
-// }
-
 export default async function HomePage() {
   const data = await fetchDataFromApi(`/api/sliders?populate=*`);
   return (
