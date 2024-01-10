1f28bf3cfe857431da15d00d829ca8472f326eb1
leasever <leandrovegaabraham@gmail.com>
2024-01-10 16:30:08 -0500

    optimized img auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,7 +10,8 @@ export default async function AuthLayout({
       <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden w-full h-full bg-muted lg:flex dark:border-r">
           <Image
-            src="./auth.webp"            
+            src="/auth.webp"
+            className="w-full object-cover"
             alt="auth"
             width={2732}
             height={2532}
