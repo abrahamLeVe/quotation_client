11c7e11498d5f1e132443959c721168ba2590ba6
leasever <leandrovegaabraham@gmail.com>
2024-01-11 18:59:39 -0500

    optimized accessibility3

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import auth from "./auth.webp";
 
 export default function AuthLayout({
   children,
@@ -11,7 +10,7 @@ export default function AuthLayout({
       <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full w-full bg-muted lg:flex dark:border-r">
           <Image
-            src={auth}
+            src="./auth.webp"
             alt="auth"
             width={1920} // Define el ancho deseado
             height={1080} // Define la altura deseada
