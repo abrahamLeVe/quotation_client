936058fa240f1e93dc4e72fa9ecac3603a33c9e2
leasever <leandrovegaabraham@gmail.com>
2024-01-11 22:23:45 -0500

    optimized imgauth4

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,22 +3,17 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const imageLoader = () => {
-    const imageUrl = `./auth.webp`;
-    return imageUrl as string;
-  };
-  console.log(imageLoader());
   return (
     <>
       <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full w-full bg-muted lg:flex dark:border-r">
-          <img
-            src={imageLoader()}
-            alt="auth"
-            loading="eager"
-            className="aspect-auto w-full object-cover"
-          />
-        </div>
+        <div
+          className="relative hidden h-full w-full bg-muted lg:flex dark:border-r"
+          style={{
+            backgroundImage: `url(./auth.webp)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
         <main>{children}</main>
       </div>
     </>
