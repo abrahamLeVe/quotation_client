8962ee5fe602eee63f74b55147a722794d087e53
leasever <leandrovegaabraham@gmail.com>
2024-01-11 21:44:01 -0500

    optimized imgauth2

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
 import Image from "next/image";
-import image from "../../../public/auth.webp";
+
+const imageLoader = () => {
+  return `../../../public/auth.webp`;
+};
 
 export default function AuthLayout({
   children,
@@ -10,7 +14,13 @@ export default function AuthLayout({
     <>
       <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full w-full bg-muted lg:flex dark:border-r">
-          <Image src={image} alt="auth" priority />
+          <Image
+            src={imageLoader.toString()}
+            alt="auth"
+            loading="eager"
+            width={1920}
+            height={1080}
+          />
         </div>
         <main>{children}</main>
       </div>
