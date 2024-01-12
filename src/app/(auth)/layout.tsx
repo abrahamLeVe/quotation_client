7aea03a033d2d839380dcc9d4f9af3311ddc3c3f
leasever <leandrovegaabraham@gmail.com>
2024-01-11 19:37:39 -0500

    optimized imgauth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import image from "../../../public/auth.webp";
 
 export default function AuthLayout({
   children,
@@ -9,13 +10,7 @@ export default function AuthLayout({
     <>
       <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full w-full bg-muted lg:flex dark:border-r">
-          <Image
-            src="./auth.webp"
-            alt="auth"
-            width={1920} // Define el ancho deseado
-            height={1080} // Define la altura deseada
-            priority
-          />
+          <Image src={image} alt="auth" priority />
         </div>
         <main>{children}</main>
       </div>
--- src/components/filter/FilterButton.tsx
+++ src/components/filter/FilterButton.tsx
@@ -6,7 +6,7 @@ import { BsMic, BsSearch } from "react-icons/bs";
 import { Icons } from "../Icons";
 
 export default function FilterButton() {
-  const { productsFilter, isListening } = useFilterContext();
+  const { isListening } = useFilterContext();
   const { startListening } = useSpeechRecognition();
 
   return (
@@ -16,7 +16,11 @@ export default function FilterButton() {
         <div className="p-2 text-gray-400 hover:text-gray-500 relative">
           <span className="sr-only">Buscar</span>
           <BsSearch className="h-6 w-6" />
-          <Link href={"/filter/search"} className="absolute inset-0" aria-label="Filtar productos"></Link>
+          <Link
+            href={"/filter/search"}
+            className="absolute inset-0"
+            aria-label="Filtar productos"
+          ></Link>
         </div>
       </div>
       <div className="flex lg:ml-6">
@@ -31,7 +35,6 @@ export default function FilterButton() {
           >
             <span className="sr-only">Escuchar</span>
             <BsMic className="h-6 w-6" aria-hidden="true" />
-            {/* <Link href={"/filter/search"} className="absolute inset-0"></Link> */}
           </button>
         )}
       </div>
