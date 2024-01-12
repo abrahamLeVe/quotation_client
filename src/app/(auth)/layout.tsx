bc086d688ee3ee1966d07fccac255d4646585b6b
leasever <leandrovegaabraham@gmail.com>
2024-01-11 22:17:06 -0500

    optimized imgauth3

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,25 +1,22 @@
-"use client";
-import Image from "next/image";
-
-const imageLoader = () => {
-  return `../../../public/auth.webp`;
-};
-
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const imageLoader = () => {
+    const imageUrl = `./auth.webp`;
+    return imageUrl as string;
+  };
+  console.log(imageLoader());
   return (
     <>
       <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full w-full bg-muted lg:flex dark:border-r">
-          <Image
-            src={imageLoader.toString()}
+          <img
+            src={imageLoader()}
             alt="auth"
             loading="eager"
-            width={1920}
-            height={1080}
+            className="aspect-auto w-full object-cover"
           />
         </div>
         <main>{children}</main>
--- src/utilities/urls.ts
+++ src/utilities/urls.ts
@@ -8,7 +8,7 @@ export const CLIENT_URL =
 
 export const OPENIA_URL = process.env.NEXT_PUBLIC_OPENIA_URL;
 
-export const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
+export const NEXTAUTH_URL = process.env.NEXTAUTH_URL || "http://localhost:3000";
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
 
