b5c44e51ebad0ee22b3b7a3f70453ffda34a63f3
unknown <leandrovegaabraham@gmail.com>
2024-04-09 23:36:00 -0500

    added limit to sizes

--- src/app/services/size.service.ts
+++ src/app/services/size.service.ts
@@ -2,6 +2,8 @@ import { fetchDataFromApi } from "@/lib/api";
 import { SizesInterface } from "@/models/size.model";
 
 export async function getDataSizes(): Promise<SizesInterface> {
-  const res = await fetchDataFromApi(`/api/sizes?populate=*&sort=name:asc`);
+  const res = await fetchDataFromApi(
+    `/api/sizes?populate=*&sort=name:asc&pagination[page]=1&pagination[pageSize]=999`
+  );
   return res;
 }
