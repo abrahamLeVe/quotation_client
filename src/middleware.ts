7c2b6f39a4f9decc71e394543f4ace4a56e69f1d
unknown <leandrovegaabraham@gmail.com>
2024-04-19 18:19:37 -0500

    resolved midleware 3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,7 @@
 import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth/next";
 import { ratelimiter } from "./lib/rate-limiter";
-import { options } from "@/app/api/auth/[...nextauth]/options";
 
 export async function middleware(req: any) {
-  const session = await getServerSession(options);
-  console.log("get token provider ", session);
-
-  if (!session) {
-    return new NextResponse("Este servicio es esclusivo para clientes.", {
-      status: 503,
-    });
-  }
-
   const ip = req.ip ?? "127.0.0.1";
   try {
     const { success } = await ratelimiter.limit(ip);
