2d75779a1b42c8b9c9a1223629bcd952ea396b69
unknown <leandrovegaabraham@gmail.com>
2024-04-30 14:53:12 -0500

    page lobby revalidate = 360

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,5 +1,6 @@
 import Slide from "@/components/slide/Slide";
 import { fetchDataFromApi } from "@/lib/api";
+export const revalidate = 360;
 
 export default async function HomePage() {
   const data = await fetchDataFromApi(`/api/sliders?populate=*`);
